52a84a4f5440090f8a7bbdf7f5fd9318bbf29e19
CorradoLanera <corrado.lanera@gmail.com>
2020-12-15 15:22:34 +0100

    setup lists

---
+++ hw4-lists/main.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+"""
+main.py: Homework #4: Lists (Python Is Easy course by Pirple)
+
+"""
+
+__author__ = "Corrado Lanera"
+__credits__ = [
+]
+__license__ = "MIT"
+__version__ = "0.0.1"
+__maintainer__ = "Corrado Lanera"
+__email__ = "corrado.lanera@gmail.com"
+
+
+# ----------------------------------------------------------------------
+
+
