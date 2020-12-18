66cb5b33bc9615cd5aa5a53f3a211ffcdae77464
CorradoLanera <corrado.lanera@gmail.com>
2020-12-18 14:36:38 +0100

    loops done

--- hw5-loops/main.py
+++ hw5-loops/main.py
@@ -60,7 +60,10 @@ You may only submit one file with maximum 100 MB in size
 """
 
 __author__ = "Corrado Lanera"
-__credits__ = []
+__credits__ = [
+    "https://www.programiz.com/python-programming/methods/list/remove",
+    "https://www.techiedelight.com/remove-first-item-from-list-python/"
+]
 __license__ = "MIT"
 __version__ = "1.0.0"
 __maintainer__ = "Corrado Lanera"
@@ -69,12 +72,64 @@ __email__ = "corrado.lanera@gmail.com"
 
 # ----------------------------------------------------------------------
 
+def has_divisor(num, div):
+    return num % div == 0
+
+
+def primes_up_to(n, silent=True):
+    to_explore = [i for i in range(2, n + 1)]
+    primes = []
+    while len(to_explore) > 0:
+        new_prime = to_explore[0]
+        # https://www.techiedelight.com/remove-first-item-from-list-python/
+        to_explore.pop(0)
+        if not silent:
+            print("I have found a new prime number:", new_prime)
+        primes.append(new_prime)
+        for i in to_explore:
+            if has_divisor(i, new_prime):
+                # https://www.programiz.com/python-programming/methods/list/remove
+                to_explore.remove(i)
+    return primes
+
+
+def fizzbuzz(silent=True):
+    res = ["" for i in range(100)]
+    for i in range(100):
+        num = i + 1
+        if has_divisor(num, 3):
+            res[i] = res[i] + "fizz"
+        if has_divisor(num, 5):
+            res[i] = res[i] + "buzz"
+        if res[i] == "":
+            res[i] = str(num)
+    for i in primes_up_to(100):
+        res[i - 1] = res[i - 1] + " (prime)"
+    res[0] = res[0] + " (is NOT a prime!)"
+    if not silent:
+        print(res)
+    return res
+
+
+fizzbuzz(silent=False)
 
 # ---- Functions' checks ----
 
 print("\n\n=== Checks starts ===\n")
 check_res = [
-    False
+    fizzbuzz()[0:6] == ["1 (is NOT a prime!)", "2 (prime)",
+                        "fizz (prime)", "4", "buzz (prime)", "fizz"
+                        ],
+    fizzbuzz()[14] == "fizzbuzz",
+    len(fizzbuzz()) == 100,
+    has_divisor(4, 2),
+    not has_divisor(5, 2),
+    has_divisor(6, 3),
+    has_divisor(10, 5),
+    has_divisor(15, 5),
+    has_divisor(15, 3),
+    primes_up_to(10) == [2, 3, 5, 7],
+    fizzbuzz()[16] == "17 (prime)"
 ]
 
 if all(check_res):
@@ -83,4 +138,3 @@ else:
     wrong = [i + 1 for i, x in enumerate(check_res) if not x]
     print("\n=== !!! Check(s) not passed:", wrong, "!!! ===\n\n")
     exit()
-
