e992b5366ea825baa63d913eeb2e155244b45e16
CorradoLanera <corrado.lanera@gmail.com>
2020-12-20 12:11:06 +0100

    Dictionaries and Sets setup

---
+++ hw7-dictionaries_and_sets/main.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+"""
+main.py: Homework #7: Dictionaries and Sets (Python Is Easy course by Pirple)
+
+"""
+
+__author__ = "Corrado Lanera"
+__credits__ = [
+    "https://stackoverflow.com/questions/366422/what-is-the-pythonic-way-to-avoid-default-parameters-that-are-empty-lists",
+    "https://note.nkmk.me/en/python-divmod-quotient-remainder/"
+]
+__license__ = "MIT"
+__version__ = "0.0.1"
+__maintainer__ = "Corrado Lanera"
+__email__ = "corrado.lanera@gmail.com"
+
+
+# ----------------------------------------------------------------------
+
+
+# ---- Functions' checks ----
+
+
+
+print("\n\n=== Checks starts ===\n")
+check_res = [
+    False
+]
+
+if all(check_res):
+    print("\n=== All checks passed ===\n\n")
+else:
+    wrong = [i + 1 for i, x in enumerate(check_res) if not x]
+    print("\n=== !!! Check(s) not passed:", wrong, "!!! ===\n\n")
+    exit()
