7a8735d0233e277d005c3661ebd50ce431fa43ae
Profeta <profeta@me.nowhere>
2021-01-03 22:29:01 +0000

    run_one_game.py: min improvements

--- proj1-connect_4/run_one_game.py
+++ proj1-connect_4/run_one_game.py
@@ -3,6 +3,10 @@ import game_play
 import display_term as display
 
 
+# this module is a bit shitty...
+# (as UI always is..............)
+
+
 def store_game(game, path):
     if path is None or path == "":
         path = "."
@@ -11,17 +15,28 @@ def store_game(game, path):
         fid.write(str(game))
 
 
+def input_choice(game):
+    # shitty fun to iterate until a valid input is selected form user
+    valid = [str(val + 1) for val in game.availables]
+    message = (
+        f"enter column number for player {display.current_symbol(game)}\n" \
+        f"(available values {valid}, u for undo and q for quit\n" \
+        f"-> "
+    )
+    while True:
+        choice = input(message).lower()
+        if choice in valid + ["u", "q"]:
+            return choice
+        else:
+            print(f"value {choice} not allowed...")
+
+
 def play(game, path=None):
     while not game.won:
         print()
         print(f"drop n. {len(game)}")
         display.print_game(game)
-        message = (
-            f"enter column number for player {display.current_symbol(game)}, " \
-            f"u for undo and q for quit\n" \
-            f"-> "
-        )
-        choice = input(message)
+        choice = input_choice(game)
         if choice.lower() == "u":
             game.undo()
             continue
@@ -33,6 +48,7 @@ def play(game, path=None):
             store_game(game, path)
 
     if game.winner != 0:
+        print()
         print(f"game ended!")
         print(f"and the winner is......... {display.winner_symbol(game)}!!! Well done!")
         print(f"final drop: ({col + 1}, {row + 1})")
